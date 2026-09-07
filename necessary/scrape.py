d343aaeecd4c3e0b332323678388d0f669437327
Gavin Oh <gavinoh@stanford.edu>
2026-09-07 15:10:31 -0400

    New shows

--- necessary/scrape.py
+++ necessary/scrape.py
@@ -83,8 +83,7 @@ with open('othershows.csv', 'a', newline='', encoding='utf-8') as f:
     
     
     urls = [
-        "https://dmbalmanac.com/TourShowSet.aspx?id=453094359&tid=8188&where=2026",
-        "https://dmbalmanac.com/TourShowSet.aspx?id=453094360&tid=8188&where=2026"
+        "https://dmbalmanac.com/TourShowSet.aspx?id=453094361&tid=8188&where=2026"
     ]
     
     for url in urls:
